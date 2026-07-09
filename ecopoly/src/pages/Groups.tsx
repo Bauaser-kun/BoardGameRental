832ca3511f2de453715b53165af9988f08bfe2b9
Bauaser-kun <michal.bala.1980@gmail.com>
2026-07-09 23:19:33 +0200

    Group grid added

---
+++ ecopoly/src/pages/Groups.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Home.css";
+import { useState } from "react";
+import GroupCard from "../components/GroupCard.tsx";
+import { COLORS } from "../constants/colors.ts";
+import "./Groups.css";
+
+function Groups() {
+    const [groups, setGroups] = useState(
+        COLORS.slice(0, 6).map((color, index) => ({
+            id: index + 1,
+            name: "",
+            color,
+        }))
+    );
+
+    const handleNameChange = (id: number, name: string) => {
+        setGroups(groups =>
+            groups.map(group =>
+                group.id === id
+                    ? { ...group, name }
+                    : group
+            )
+        );
+    };
+
+    const handleColorChange = (id: number, color: string) => {
+        setGroups(groups =>
+            groups.map(group =>
+                group.id === id
+                    ? { ...group, color }
+                    : group
+            )
+        );
+    };
+
+  return (
+    <div className="mainPage">
+        <h1>Group selection</h1>
+        <div className="groups-Grid">
+            {groups.map(group => (
+                <GroupCard
+                    key={group.id}
+                    group={group}
+                    groups={groups}
+                    onNameChange={handleNameChange}
+                    onColorChange={handleColorChange}
+                />
+            ))}
+        </div>
+
+        <div className="progress-view">
+            
+        </div>
+    </div>
+
+  );
+}
+
+export default Groups;
